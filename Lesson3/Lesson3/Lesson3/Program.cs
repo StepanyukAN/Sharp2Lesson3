a6d7b44586ce3506d20e7950893d3eb6efceab7d
StepanyukAN <fargogi@mail.ru>
2018-10-10 23:21:28 +1000

    Решена задача 5

---
+++ Lesson3/Lesson3/Lesson3/Program.cs
@@ -0,0 +1,46 @@
+﻿//* Добавить в пример Lesson3 обобщенный делегат.
+using System;
+namespace Delegates_Observer
+{
+    public delegate void MyDelegate<T>(T o);
+    class Source
+    {
+        public event MyDelegate<Source> Run;
+
+        public void Start()
+        {
+            Console.WriteLine("RUN");
+            if (Run != null) Run(this);
+        }
+    }
+    class Observer1 // Наблюдатель 1
+    {
+        public void Do(object o)
+        {
+            Console.WriteLine("Первый. Принял, что объект {0} побежал", o);
+        }
+    }
+    class Observer2 // Наблюдатель 2
+    {
+        public void Do(object o)
+        {
+            Console.WriteLine("Второй. Принял, что объект {0} побежал", o);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Source s = new Source();
+            Observer1 o1 = new Observer1();
+            Observer2 o2 = new Observer2();
+            MyDelegate<Source> d1 = new MyDelegate<Source>(o1.Do);
+            s.Run += d1;
+            s.Run += o2.Do;
+            s.Start();
+            s.Run -= d1;
+            s.Start();
+            Console.ReadKey();
+        }
+    }
+}
